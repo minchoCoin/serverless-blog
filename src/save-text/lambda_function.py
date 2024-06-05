31479732a3a834784df42eade66069b5d193e35a
Taehun Kim <bigteach0508@pusan.ac.kr>
2024-06-06 00:04:43 +0900

    little update to save_text

--- src/save-text/lambda_function.py
+++ src/save-text/lambda_function.py
@@ -12,13 +12,15 @@ def lambda_handler(event, context):
     job_name = event['detail']['TranscriptionJobName']
     job = transcribe.get_transcription_job(TranscriptionJobName=job_name)
     uri = job['TranscriptionJob']['Transcript']['TranscriptFileUri']
-    print(uri)
+    #print(uri)
     
     content = urllib.request.urlopen(uri).read().decode('UTF-8')
-    print(json.dumps(content))
+    #write content to cloudwatch logs
+    #print(json.dumps(content))
     
     data =  json.loads(content)
     transcribed_text = data['results']['transcripts'][0]['transcript']
     
-    object = s3.Object(BUCKET_NAME, job_name + ".txt")
+    #job_name이 abc.mp3 형태이기 때문에, job_name에서 mp3를 제외하고 .txt를 붙이겠습니다.
+    object = s3.Object(BUCKET_NAME, f'output_text/{job_name.replace('.mp3', '')}.txt')
     object.put(Body=transcribed_text)
